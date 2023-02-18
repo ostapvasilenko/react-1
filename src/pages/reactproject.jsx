e956966354bb5bca05d01f087e0916818b387252
ostapvasulenko <ostapvasilenko55@gmail.com>
2023-02-18 14:33:05 +0200

    1

--- src/pages/reactproject.jsx
+++ src/pages/reactproject.jsx
@@ -1,17 +1,40 @@
-import React from 'react'
+import React, { useState } from "react";
+import styles from "../styles/Reactproject.module.css";
 
-const student = () => {
-    return (
-        <div>
-            <h2>REACT PROJECT</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus nobis cupiditate, assumenda rem illo architecto.</p>
-            <button>Add</button>
-            <button>Save</button>
-            <button>Delete</button>
+const Student = () => {
+  let [counter, setCounter] = useState(0);
+
+  return (
+    <>
+      <div className="container">
+        <div className="row">
+          <div className="col-12">
+            <h1 className={styles.title}>Student Page</h1>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magnam
+              aut rem, laudantium quia veritatis minima esse iure eos at officia
+              odit corporis ab sint repellendus sit eius quibusdam officiis
+              possimus!
+            </p>
+            <div
+              className="btn btn-success"
+              onClick={() => {
+                setCounter(counter + 1);
+              }}
+            >
+              Add +1
+            </div>
+            <h2>Result: {counter}</h2>
+            <form action="">
+                <input type="text" placeholder="Name"/>
+                <input type="email" placeholder="Email"/>
+            </form>
+          </div>
         </div>
+      </div>
+    </>
+  );
+};
 
-    )
-}
-
-export default student
+export default Student;
 
